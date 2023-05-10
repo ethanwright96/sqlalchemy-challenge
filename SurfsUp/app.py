b7d3948942ecfdcf6e2ec09113b6f9a442992c1b
Ethan Wright <ethan_wright@alumni.brown.edu>
2023-05-10 12:16:49 -0400

    started flask app completed precipitation

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,12 +1,145 @@
 # Import the dependencies.
+import numpy as np
 
-# Note: always close session after retrieval of data.
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
 
-session.close()
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+conn = engine.connect()
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save references to the table(s)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Flask Setup
+#################################################
+app = Flask(__name__)
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        # f"/api/v1.0/stations<br/>"
+        # f"/api/v1.0/tobs<br/>"
+        # f"/api/v1.0/<start><br/>"
+        # f"/api/v1.0/<start>/<end><br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    
+    # Define the start of the period of interest
+    period_start = dt.date(2017,8,23) - dt.timedelta(days=365)
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query date and prcp for the last 12 months
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= period_start).all()
+    
+    # Close session
+    session.close()
+    
+    # Convert data into a dictionary
+    data = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        data.append(prcp_dict)
+    
+    return jsonify(data)
+
+# @app.route("/api/v1.0/stations")
+# def names():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of all passenger names"""
+#     # Query all passengers
+#     results = session.query(Passenger.name).all()
+
+#     session.close()
+
+#     # Convert list of tuples into normal list
+#     all_names = list(np.ravel(results))
+
+#     return jsonify(all_names)
+
+# @app.route("/api/v1.0/tobs")
+# def names():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of all passenger names"""
+#     # Query all passengers
+#     results = session.query(Passenger.name).all()
+
+#     session.close()
+
+#     # Convert list of tuples into normal list
+#     all_names = list(np.ravel(results))
+
+#     return jsonify(all_names)
+
+# @app.route("/api/v1.0/<start>")
+# def names():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of all passenger names"""
+#     # Query all passengers
+#     results = session.query(Passenger.name).all()
+
+#     session.close()
+
+#     # Convert list of tuples into normal list
+#     all_names = list(np.ravel(results))
+
+#     return jsonify(all_names)
+
+# @app.route("/api/v1.0/<start>/<end>")
+# def names():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of all passenger names"""
+#     # Query all passengers
+#     results = session.query(Passenger.name).all()
+
+#     session.close()
+
+#     # Convert list of tuples into normal list
+#     all_names = list(np.ravel(results))
+
+#     return jsonify(all_names)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# Note: always close session after retrieval of data.
+
+#session.close()
 
 
 # reflect an existing database into a new model
